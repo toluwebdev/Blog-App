7c011d4c30babebce198086add494c82cd467468
Tolu Oladoja <oladojatolu@gmail.com>
2025-11-02 07:33:16 +0100

    Fix layout and class names in Comments component

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -30,9 +30,9 @@ const Comments = () => {
   }, []);
   return (
     <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
-      <div className="flex justify-between items-center max-w-3xl">
+      <div className="flex flex-wrap gap-3 justify-between items-center max-w-3xl">
         <h1>Comments</h1>
-        <div className="flex gap-4">
+        <div className="flex fllex-wrap gap-4">
           <button
             onClick={() => setFilter("Approved")}
             className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${
